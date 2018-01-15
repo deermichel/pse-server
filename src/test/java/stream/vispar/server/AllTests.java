b244ec88239e67e0b98cc5afafc70f8483f47628
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-01-15 07:07:25 +0100

    Add MultiLogger test + test suite

---
+++ src/test/java/stream/vispar/server/AllTests.java
@@ -0,0 +1,37 @@
+package stream.vispar.server;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import stream.vispar.server.cli.DefaultConsoleTest;
+import stream.vispar.server.core.ServerConfigTest;
+import stream.vispar.server.localization.LocalizerTest;
+import stream.vispar.server.logger.ConsoleLoggerTest;
+import stream.vispar.server.logger.FileLoggerTest;
+import stream.vispar.server.logger.MultiLoggerTest;
+
+/**
+ * Test suite for all tests.
+ * 
+ * @author Micha Hanselmann
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+    
+    // cli
+    DefaultConsoleTest.class,
+    
+    // core
+    ServerConfigTest.class,
+    
+    // localization
+    LocalizerTest.class,
+    
+    // logger
+    ConsoleLoggerTest.class,
+    FileLoggerTest.class,
+    MultiLoggerTest.class
+    
+})
+public class AllTests { }
---
+++ src/test/java/stream/vispar/server/logger/MultiLoggerTest.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package stream.vispar.server.logger;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.stringContainsInOrder;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for {@link MultiLogger}.
+ * 
+ * @author Micha Hanselmann
+ */
+public class MultiLoggerTest {
+
+    /**
+     * Logger implementation for testing purposes
+     */
+    private class TestLogger implements ILogger {
+        
+        private String log = "";
+        
+        @Override
+        public void log(String message) {
+            log += message + "\n";
+        }
+    
+        @Override
+        public void logError(String error) {
+            log += "[ERROR] " + error + "\n";
+        }
+        
+        public String getLog() {
+            return log;
+        }
+    }
+    
+
+    /**
+     * Test method for {@link MultiLogger#log(String)}.
+     */
+    @Test
+    public void testLog() {
+        TestLogger t1 = new TestLogger();
+        TestLogger t2 = new TestLogger();
+        MultiLogger multi = new MultiLogger();
+        multi.addLogger(t1);
+        multi.addLogger(t2);
+        
+        multi.log("Hello Log.");
+        assertThat(t1.getLog(), containsString("Hello Log.\n"));
+        assertThat(t2.getLog(), containsString("Hello Log.\n"));
+    }
+
+    /**
+     * Test method for {@link MultiLogger#logError(String)}.
+     */
+    @Test
+    public void testLogError() {
+        TestLogger t1 = new TestLogger();
+        TestLogger t2 = new TestLogger();
+        MultiLogger multi = new MultiLogger();
+        multi.addLogger(t1);
+        multi.addLogger(t2);
+        
+        multi.logError("Hello Error!");
+        assertThat(t1.getLog(), containsString("[ERROR] Hello Error!\n"));
+        assertThat(t2.getLog(), containsString("[ERROR] Hello Error!\n"));
+    }
+
+    /**
+     * Test method for {@link MultiLogger#addLogger(ILogger)}.
+     * 
+     * Checks null argument behaviour.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testAddLoggerNull() {
+        new MultiLogger().addLogger(null);
+    }
+}
