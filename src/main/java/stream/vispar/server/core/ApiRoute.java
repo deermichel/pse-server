848671597ff7506a2a19111481903e3ee267481b
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-01-20 08:11:08 +0100

    Routes refactoring

--- src/main/java/stream/vispar/server/core/ApiRoute.java
+++ src/main/java/stream/vispar/server/core/ApiRoute.java
@@ -1,14 +1,10 @@
 package stream.vispar.server.core;
 
-import java.util.ArrayList;
 import java.util.Collection;
-
-import com.google.gson.JsonObject;
 
 import stream.vispar.jsonconverter.IJsonConverter;
 import stream.vispar.jsonconverter.exceptions.JsonException;
 import stream.vispar.jsonconverter.exceptions.JsonParseException;
-import stream.vispar.jsonconverter.exceptions.JsonSyntaxException;
 import stream.vispar.jsonconverter.gson.GsonConverter;
 import stream.vispar.jsonconverter.gson.typeadapters.GsonJsonArray;
 import stream.vispar.jsonconverter.gson.typeadapters.GsonJsonObject;
@@ -16,7 +12,6 @@ import stream.vispar.jsonconverter.types.IJsonArray;
 import stream.vispar.jsonconverter.types.IJsonElement;
 import stream.vispar.jsonconverter.types.IJsonObject;
 import stream.vispar.model.Pattern;
-import stream.vispar.server.core.entities.User;
 
 /**
  * Defines the routes for the api server provided by a {@link IRequestHandler}.
@@ -33,21 +28,21 @@ public enum ApiRoute {
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             IJsonObject response = new GsonJsonObject();
             try {
-                // parse json
+                
+                // get username and password
                 String password = request.getAsJsonObject().get("data")
                         .getAsJsonObject().getAsJsonPrimitive("password").getAsString();
                 String username = request.getAsJsonObject().get("data")
                         .getAsJsonObject().getAsJsonPrimitive("username").getAsString();
                 
-                // login and return token if successful
-                try {
-                    String token = instance.getAuthMgr().login(username, password);
-                    IJsonObject data = new GsonJsonObject();
-                    data.add("token", token);
-                    response.add("data", data);
-                } catch (IllegalArgumentException e) {
-                    response.add("error", RouteError.UNKNOWN_CREDENTIALS.getCode());
-                }
+                // login and return token
+                String token = instance.getAuthMgr().login(username, password);
+                IJsonObject data = new GsonJsonObject();
+                data.add("token", token);
+                response.add("data", data);
+                    
+            } catch (IllegalArgumentException e) {
+                response.add("error", RouteError.UNKNOWN_CREDENTIALS.getCode());
             } catch (JsonParseException | NullPointerException e) {
                 instance.getLogger().logError(e.toString());
                 response.add("error", RouteError.INVALID_REQUEST.getCode());
@@ -63,11 +58,13 @@ public enum ApiRoute {
         @Override
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             try {
+                
                 // logout if user was logged in
-                if (request.getAsJsonObject().has("user")) {
+                if (isAuthenticated(instance, request)) {
                     String token = request.getAsJsonObject().getAsJsonPrimitive("token").getAsString();
                     instance.getAuthMgr().logout(token);
                 }
+                
             } catch (JsonParseException | NullPointerException e) {
                 instance.getLogger().logError(e.toString()); // who cares?
             }
@@ -82,26 +79,23 @@ public enum ApiRoute {
         @Override
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             IJsonObject response = new GsonJsonObject();
-            try {
-                // authenticated?
-                if (request.getAsJsonObject().has("user")) {
-                    
-                    // get patterns and convert their proxies to json
-                    IJsonConverter jsonConv = new GsonConverter();
-                    Collection<Pattern> patterns = instance.getPatternCtrl().getAll();
-                    IJsonArray json = new GsonJsonArray();
-                    for (Pattern p : patterns) {
-                        json.add(jsonConv.toJson(p.getAsProxy()));
-                    }
-                    
-                    response.add("data", json); // add to response
-                } else {
-                    response.add("error", RouteError.NOT_AUTHORIZED.getCode());
-                }
-            } catch (JsonParseException | NullPointerException e) {
-                instance.getLogger().logError(e.toString());
-                response.add("error", RouteError.INVALID_REQUEST.getCode());
+            
+            // authenticated?
+            if (!isAuthenticated(instance, request)) {
+                response.add("error", RouteError.NOT_AUTHORIZED.getCode());
+                return response;
             }
+            
+            // get patterns and convert their proxies to json
+            IJsonConverter jsonConv = new GsonConverter();
+            Collection<Pattern> patterns = instance.getPatternCtrl().getAll();
+            IJsonArray json = new GsonJsonArray();
+            for (Pattern p : patterns) {
+                json.add(jsonConv.toJson(p.getAsProxy()));
+            }
+            
+            // return data
+            response.add("data", json); 
             return response;
         }
     },
@@ -113,24 +107,27 @@ public enum ApiRoute {
         @Override
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             IJsonObject response = new GsonJsonObject();
+            
+            // authenticated?
+            if (!isAuthenticated(instance, request)) {
+                response.add("error", RouteError.NOT_AUTHORIZED.getCode());
+                return response;
+            }
+            
             try {
-                // authenticated?
-                if (request.getAsJsonObject().has("user")) {
                     
-                    // get pattern
-                    String patternId = request.getAsJsonObject().get("params")
-                            .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
-                    Pattern pattern = instance.getPatternCtrl().getById(patternId);
-                    
-                    // return pattern if it exists
-                    if (pattern != null) {
-                        response.add("data", new GsonConverter().toJson(pattern));
-                    } else {
-                        response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
-                    }
+                // get pattern
+                String patternId = request.getAsJsonObject().get("params")
+                        .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
+                Pattern pattern = instance.getPatternCtrl().getById(patternId);
+                
+                // return pattern if it exists
+                if (pattern != null) {
+                    response.add("data", new GsonConverter().toJson(pattern));
                 } else {
-                    response.add("error", RouteError.NOT_AUTHORIZED.getCode());
+                    response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
                 }
+                
             } catch (JsonParseException | NullPointerException e) {
                 instance.getLogger().logError(e.toString());
                 response.add("error", RouteError.INVALID_REQUEST.getCode());
@@ -146,24 +143,25 @@ public enum ApiRoute {
         @Override
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             IJsonObject response = new GsonJsonObject();
+            
+            // authenticated?
+            if (!isAuthenticated(instance, request)) {
+                response.add("error", RouteError.NOT_AUTHORIZED.getCode());
+                return response;
+            }
+            
             try {
-                // authenticated?
-                if (request.getAsJsonObject().has("user")) {
                     
-                    // get pattern
-                    IJsonConverter jsonConv = new GsonConverter();
-                    Pattern pattern = jsonConv.fromJson(request.getAsJsonObject().get("data"));
-                    
-                    // update pattern
-                    try {
-                        pattern = instance.getPatternCtrl().update(pattern);
-                        response.add("data", jsonConv.toJson(pattern));
-                    } catch (IllegalArgumentException e) {
-                        response.add("error", RouteError.PATTERN_EDITED.getCode());
-                    }
-                } else {
-                    response.add("error", RouteError.NOT_AUTHORIZED.getCode());
-                }
+                // get pattern
+                IJsonConverter jsonConv = new GsonConverter();
+                Pattern pattern = jsonConv.fromJson(request.getAsJsonObject().get("data"));
+                
+                // update pattern
+                pattern = instance.getPatternCtrl().update(pattern);
+                response.add("data", jsonConv.toJson(pattern));
+                
+            } catch (IllegalArgumentException e) {
+                response.add("error", RouteError.PATTERN_EDITED.getCode());
             } catch (JsonException | NullPointerException e) {
                 instance.getLogger().logError(e.toString());
                 response.add("error", RouteError.INVALID_REQUEST.getCode());
@@ -179,23 +177,24 @@ public enum ApiRoute {
         @Override
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             IJsonObject response = new GsonJsonObject();
+            
+            // authenticated?
+            if (!isAuthenticated(instance, request)) {
+                response.add("error", RouteError.NOT_AUTHORIZED.getCode());
+                return response;
+            }
+            
             try {
-                // authenticated?
-                if (request.getAsJsonObject().has("user")) {
                     
-                    // get pattern id
-                    String patternId = request.getAsJsonObject().get("data")
-                            .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
-                    
-                    // remove pattern if exists
-                    try {
-                        instance.getPatternCtrl().remove(patternId);
-                    } catch (IllegalArgumentException e) {
-                        response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
-                    }
-                } else {
-                    response.add("error", RouteError.NOT_AUTHORIZED.getCode());
-                }
+                // get pattern id
+                String patternId = request.getAsJsonObject().get("data")
+                        .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
+                
+                // remove pattern
+                instance.getPatternCtrl().remove(patternId);
+                
+            } catch (IllegalArgumentException e) {
+                response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
             } catch (JsonException | NullPointerException e) {
                 instance.getLogger().logError(e.toString());
                 response.add("error", RouteError.INVALID_REQUEST.getCode());
@@ -211,26 +210,27 @@ public enum ApiRoute {
         @Override
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             IJsonObject response = new GsonJsonObject();
+            
+            // authenticated?
+            if (!isAuthenticated(instance, request)) {
+                response.add("error", RouteError.NOT_AUTHORIZED.getCode());
+                return response;
+            }
+            
             try {
-                // authenticated?
-                if (request.getAsJsonObject().has("user")) {
                     
-                    // get pattern id
-                    String patternId = request.getAsJsonObject().get("data")
-                            .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
-                    
-                    // deploy pattern
-                    try {
-                        Pattern pattern = instance.getPatternCtrl().deploy(patternId);
-                        response.add("data", new GsonConverter().toJson(pattern));
-                    } catch (IllegalArgumentException e) {
-                        response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
-                    } catch (IllegalStateException e) {
-                        response.add("error", RouteError.PATTERN_ALREADY_DEPLOYED.getCode());
-                    }
-                } else {
-                    response.add("error", RouteError.NOT_AUTHORIZED.getCode());
-                }
+                // get pattern id
+                String patternId = request.getAsJsonObject().get("data")
+                        .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
+                
+                // deploy pattern
+                Pattern pattern = instance.getPatternCtrl().deploy(patternId);
+                response.add("data", new GsonConverter().toJson(pattern));
+                
+            } catch (IllegalArgumentException e) {
+                response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
+            } catch (IllegalStateException e) {
+                response.add("error", RouteError.PATTERN_ALREADY_DEPLOYED.getCode());
             } catch (JsonException | NullPointerException e) {
                 instance.getLogger().logError(e.toString());
                 response.add("error", RouteError.INVALID_REQUEST.getCode());
@@ -246,26 +246,27 @@ public enum ApiRoute {
         @Override
         public IJsonElement execute(ServerInstance instance, IJsonElement request) {
             IJsonObject response = new GsonJsonObject();
+            
+            // authenticated?
+            if (!isAuthenticated(instance, request)) {
+                response.add("error", RouteError.NOT_AUTHORIZED.getCode());
+                return response;
+            }
+            
             try {
-                // authenticated?
-                if (request.getAsJsonObject().has("user")) {
                     
-                    // get pattern id
-                    String patternId = request.getAsJsonObject().get("data")
-                            .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
-                    
-                    // undeploy pattern
-                    try {
-                        Pattern pattern = instance.getPatternCtrl().undeploy(patternId);
-                        response.add("data", new GsonConverter().toJson(pattern));
-                    } catch (IllegalArgumentException e) {
-                        response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
-                    } catch (IllegalStateException e) {
-                        response.add("error", RouteError.PATTERN_NOT_DEPLOYED.getCode());
-                    }
-                } else {
-                    response.add("error", RouteError.NOT_AUTHORIZED.getCode());
-                }
+                // get pattern id
+                String patternId = request.getAsJsonObject().get("data")
+                        .getAsJsonObject().getAsJsonPrimitive("id").getAsString();
+                
+                // undeploy pattern
+                Pattern pattern = instance.getPatternCtrl().undeploy(patternId);
+                response.add("data", new GsonConverter().toJson(pattern));
+                
+            } catch (IllegalArgumentException e) {
+                response.add("error", RouteError.UNKNOWN_PATTERN.getCode());
+            } catch (IllegalStateException e) {
+                response.add("error", RouteError.PATTERN_NOT_DEPLOYED.getCode());
             } catch (JsonException | NullPointerException e) {
                 instance.getLogger().logError(e.toString());
                 response.add("error", RouteError.INVALID_REQUEST.getCode());
@@ -327,6 +328,25 @@ public enum ApiRoute {
      */
     public RouteType getType() {
         return type;
+    }
+    
+    /**
+     * Checks wether the request is authenticated (a user is logged in).
+     * 
+     * @param request
+     *          the request.   
+     * @param instance
+     *          the currently used {@link ServerInstance}.
+     * @return
+     *          true if authenticated, false otherwise.
+     */
+    protected final boolean isAuthenticated(ServerInstance instance, IJsonElement request) {
+        try {
+            return request.getAsJsonObject().has("user");
+        } catch (JsonParseException e) {
+            instance.getLogger().logError(e.toString());
+        }
+        return false;
     }
     
     /**
