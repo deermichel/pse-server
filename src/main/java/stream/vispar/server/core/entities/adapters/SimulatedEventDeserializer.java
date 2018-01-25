5d255682808ebf9549fdb1c3980fdfa16f25b77d
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-01-25 08:14:54 +0100

    Introducing simulation file validation

--- src/main/java/stream/vispar/server/core/entities/adapters/SimulatedEventDeserializer.java
+++ src/main/java/stream/vispar/server/core/entities/adapters/SimulatedEventDeserializer.java
@@ -32,26 +32,43 @@ public class SimulatedEventDeserializer implements JsonDeserializer<SimulatedEve
         }
         JsonObject jsonObj = json.getAsJsonObject();
 
-        // validate sensor config json
-//        if (!jsonObj.getAsJsonPrimitive("name").getAsString().matches("[a-zA-Z0-9]+")) {
-//            throw new JsonParseException("Name has to match regex [a-zA-Z0-9]+");
-//        } else if (!jsonObj.getAsJsonPrimitive("endpoint").getAsString().matches("[a-zA-Z0-9]+")) {
-//            throw new JsonParseException("Endpoint has to match regex [a-zA-Z0-9]+");
-//        }
-//
-//        String attrRegex = "[a-zA-Z0-9]+(\\[[0-9]+\\])?(\\.[a-zA-Z0-9]+(\\[[0-9]+\\])?)*";
-//
-//        for (Entry<String, JsonElement> attr : jsonObj.getAsJsonObject("attributes").entrySet()) {
-//            if (!attr.getKey().matches(attrRegex)) {
-//                throw new JsonParseException("Attributes have to match regex " + attrRegex);
-//            } else if (!attr.getValue().getAsJsonObject().getAsJsonPrimitive("name").getAsString()
-//                    .matches("[a-zA-Z0-9]+")) {
-//                throw new JsonParseException("Attribute names have to match regex [a-zA-Z0-9]+");
-//            } else if (!attr.getValue().getAsJsonObject().getAsJsonPrimitive("type").getAsString()
-//                    .matches("INTEGER|DOUBLE|STRING")) {
-//                throw new JsonParseException("Attribute types have to be INTEGER, DOUBLE or STRING");
-//            }
-//        }
+        // validate simulated event json
+        if (!jsonObj.getAsJsonPrimitive("time").getAsString().matches("[0-9]+")) {
+            throw new JsonParseException("Time has to match regex [0-9]+");
+        } else if (!jsonObj.getAsJsonPrimitive("sensor").getAsString().matches("[a-zA-Z0-9]+")) {
+            throw new JsonParseException("Sensor has to match regex [a-zA-Z0-9]+");
+        }
+        
+        for (Entry<String, JsonElement> data : jsonObj.getAsJsonObject("data").entrySet()) {
+            if (!data.getKey().matches("[a-zA-Z0-9]+")) {
+                throw new JsonParseException("Attribute names have to match regex [a-zA-Z0-9]+");
+            }
+            JsonObject value = data.getValue().getAsJsonObject();
+            if (!(value.has("fixed") || value.has("range") || value.has("drange") || value.has("random"))) {
+                throw new JsonParseException("Value has to be either fixed, range, drange or random");
+            }
+            if (value.has("fixed") && !value.get("fixed").isJsonPrimitive()) {
+                throw new JsonParseException("Fixed value has to be a primitive");
+            } else if (value.has("range")) {
+                if (value.getAsJsonArray("range").size() != 2) {
+                    throw new JsonParseException("Range has to be an array with size 2");
+                } else if (!value.getAsJsonArray("range").get(0).getAsString().matches("[0-9]+")) {
+                    throw new JsonParseException("Range minimum has to match regex [0-9]+");
+                } else if (!value.getAsJsonArray("range").get(1).getAsString().matches("[0-9]+")) {
+                    throw new JsonParseException("Range maximum has to match regex [0-9]+");
+                }
+            } else if (value.has("drange")) {
+                if (value.getAsJsonArray("drange").size() != 2) {
+                    throw new JsonParseException("Double range has to be an array with size 2");
+                } else if (!value.getAsJsonArray("drange").get(0).getAsString().matches("[0-9\\.]+")) {
+                    throw new JsonParseException("Double range minimum has to match regex [0-9\\.]+");
+                } else if (!value.getAsJsonArray("drange").get(1).getAsString().matches("[0-9\\.]+")) {
+                    throw new JsonParseException("Double range maximum has to match regex [0-9\\.]+");
+                }
+            } else if (value.has("random") && !value.get("random").isJsonArray()) {
+                throw new JsonParseException("Random value has to offer an array of options");
+            }
+        }
 
         // all checks successful - do conversion
         return new GsonBuilder().create().fromJson(jsonObj, typeOfT);
