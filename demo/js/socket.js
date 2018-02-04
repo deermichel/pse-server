ff42d7f57812ca94013723c069842a8f9641be66
Micha Hanselmann <DeerMichel@users.noreply.github.com>
2018-02-04 20:05:12 +0100

    Add demo socket js

---
+++ demo/js/socket.js
@@ -0,0 +1,42 @@
+const gpio = require("rpi-gpio")
+const WebSocket = require("ws")
+const connection = new WebSocket("ws://mh-macbook.local:8081")
+
+// pins
+const led_blue = 23
+const led_green = 19
+const buzzer = 15
+
+// setup gpio
+gpio.reset()
+gpio.setup(led_blue, gpio.DIR_OUT)
+gpio.setup(led_green, gpio.DIR_OUT)
+gpio.setup(buzzer, gpio.DIR_OUT)
+
+// message handler
+connection.onmessage = (e) => {
+    console.log("Received: " + e.data)
+    if (e.data.startsWith("led.blue.")) {
+        let duration = e.data.substring(9)
+        gpio.write(led_blue, true)
+        setTimeout(() => gpio.write(led_blue, false), duration)
+    } else if (e.data.startsWith("led.green.")) {
+        let duration = e.data.substring(10)
+        gpio.write(led_green, true)
+        setTimeout(() => gpio.write(led_green, false), duration)
+    } else if (e.data.startsWith("buzzer.")) {
+        let duration = e.data.substring(7)
+        gpio.write(buzzer, true)
+        setTimeout(() => gpio.write(buzzer, false), duration)
+    }
+}
+
+// error
+connection.onerror = (error) => {
+    console.log(error)
+}
+
+// connected
+connection.onopen = (error) => {
+    console.log("Connected.")
+}
