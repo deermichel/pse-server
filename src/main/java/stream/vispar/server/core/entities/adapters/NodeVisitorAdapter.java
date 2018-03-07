036d8e2b7282ef264ac8edcf5701f2612a2e16b9
Nico Weidmann <nicoweidmann@web.de>
2018-03-07 18:27:05 +0100

    Update NodeVisitorAdapter to represent changed model
    Added methods to the NodeVisitorAdapter to avoid having to implement
    them when not needed.

--- src/main/java/stream/vispar/server/core/entities/adapters/NodeVisitorAdapter.java
+++ src/main/java/stream/vispar/server/core/entities/adapters/NodeVisitorAdapter.java
@@ -2,12 +2,14 @@ package stream.vispar.server.core.entities.adapters;
 
 import stream.vispar.model.NodeVisitor;
 import stream.vispar.model.nodes.inputs.ConstantInputNode;
+import stream.vispar.model.nodes.inputs.PatternInputNode;
 import stream.vispar.model.nodes.inputs.SensorNode;
 import stream.vispar.model.nodes.operators.CountAggregationNode;
 import stream.vispar.model.nodes.operators.FilterOperatorNode;
 import stream.vispar.model.nodes.operators.FunctionAggregationNode;
 import stream.vispar.model.nodes.operators.LogicalOperatorNode;
 import stream.vispar.model.nodes.outputs.MailActionNode;
+import stream.vispar.model.nodes.outputs.PatternOutputNode;
 import stream.vispar.model.nodes.outputs.SocketActionNode;
 
 /**
@@ -56,6 +58,16 @@ public abstract class NodeVisitorAdapter implements NodeVisitor {
 
     @Override
     public void visitLogicalOperatorNode(LogicalOperatorNode node) {
+        // does nothing
+    }
+    
+    @Override
+    public void visitPatternInputNode(PatternInputNode node) {
+        // does nothing
+    }
+    
+    @Override
+    public void visitPatternOutputNode(PatternOutputNode node) {
         // does nothing
     }
 }
