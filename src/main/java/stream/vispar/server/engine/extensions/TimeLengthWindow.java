0addd612ec37a5925960f9b3f79c993fc4f40f38
Jonas <ufvsd@student.kit.edu>
2018-03-10 16:25:51 +0100

    Fix TimeLengthWindow to work properly

--- src/main/java/stream/vispar/server/engine/extensions/TimeLengthWindow.java
+++ src/main/java/stream/vispar/server/engine/extensions/TimeLengthWindow.java
@@ -1,6 +1,5 @@
 package stream.vispar.server.engine.extensions;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -8,19 +7,21 @@ import org.wso2.siddhi.core.config.ExecutionPlanContext;
 import org.wso2.siddhi.core.event.ComplexEvent;
 import org.wso2.siddhi.core.event.ComplexEventChunk;
 import org.wso2.siddhi.core.event.MetaComplexEvent;
-import org.wso2.siddhi.core.event.stream.MetaStreamEvent;
 import org.wso2.siddhi.core.event.stream.StreamEvent;
 import org.wso2.siddhi.core.event.stream.StreamEventCloner;
+import org.wso2.siddhi.core.executor.ConstantExpressionExecutor;
 import org.wso2.siddhi.core.executor.ExpressionExecutor;
 import org.wso2.siddhi.core.executor.VariableExpressionExecutor;
 import org.wso2.siddhi.core.query.processor.Processor;
+import org.wso2.siddhi.core.query.processor.SchedulingProcessor;
 import org.wso2.siddhi.core.query.processor.stream.window.FindableProcessor;
-import org.wso2.siddhi.core.query.processor.stream.window.LengthWindowProcessor;
-import org.wso2.siddhi.core.query.processor.stream.window.TimeWindowProcessor;
 import org.wso2.siddhi.core.query.processor.stream.window.WindowProcessor;
 import org.wso2.siddhi.core.table.EventTable;
+import org.wso2.siddhi.core.util.Scheduler;
 import org.wso2.siddhi.core.util.collection.operator.Finder;
-import org.wso2.siddhi.query.api.definition.AbstractDefinition;
+import org.wso2.siddhi.core.util.parser.CollectionOperatorParser;
+import org.wso2.siddhi.query.api.definition.Attribute;
+import org.wso2.siddhi.query.api.exception.ExecutionPlanValidationException;
 import org.wso2.siddhi.query.api.expression.Expression;
 
 /**
@@ -28,105 +29,134 @@ import org.wso2.siddhi.query.api.expression.Expression;
  * 
  * @author Jonas
  */
-public class TimeLengthWindow extends WindowProcessor implements FindableProcessor {
+public class TimeLengthWindow extends WindowProcessor implements SchedulingProcessor, FindableProcessor {
+	
+	private int length;
+    private int count = 0;
+    private long timeInMilliSeconds;
+    private ComplexEventChunk<StreamEvent> expiredEventChunk;
+    private Scheduler scheduler;
+    private ExecutionPlanContext executionPlanContext;
 
-    private LengthWindowProcessor lengthWindow = new LengthWindowProcessor();
-    private TimeWindowProcessor timeWindow = new TimeWindowProcessor();
-
-    public TimeLengthWindow() {
-        lengthWindow.setNextProcessor(timeWindow);
-        timeWindow.setNextProcessor(this);
+    public void setTimeInMilliSeconds(long timeInMilliSeconds) {
+        this.timeInMilliSeconds = timeInMilliSeconds;
     }
 
     @Override
-    public AbstractDefinition initProcessor(AbstractDefinition inputDefinition,
-            ExpressionExecutor[] attributeExpressionExecutors, ExecutionPlanContext executionPlanContext) {
-        lengthWindow.initProcessor(inputDefinition, new ExpressionExecutor[] { attributeExpressionExecutors[1] },
-                executionPlanContext);
-        timeWindow.initProcessor(inputDefinition, new ExpressionExecutor[] { attributeExpressionExecutors[0] },
-                executionPlanContext);
-        return super.initProcessor(inputDefinition, attributeExpressionExecutors, executionPlanContext);
+    public void setScheduler(Scheduler scheduler) {
+        this.scheduler = scheduler;
     }
 
     @Override
-    public void process(@SuppressWarnings("rawtypes") ComplexEventChunk complexEventChunk) {
-        super.process(complexEventChunk);
-        lengthWindow.process(complexEventChunk);
-        timeWindow.process(complexEventChunk);
+    public Scheduler getScheduler() {
+        return scheduler;
     }
 
     @Override
-    public Processor cloneProcessor(String key) {
-        TimeLengthWindow clone = (TimeLengthWindow) super.cloneProcessor(key);
-        clone.timeWindow = (TimeWindowProcessor) timeWindow.cloneProcessor(key);
-        clone.lengthWindow = (LengthWindowProcessor) lengthWindow.cloneProcessor(key);
-        return clone;
+	protected void init(ExpressionExecutor[] attributeExpressionExecutors, ExecutionPlanContext executionPlanContext) {
+    	this.executionPlanContext = executionPlanContext;
+        this.expiredEventChunk = new ComplexEventChunk<StreamEvent>();
+        
+		if (attributeExpressionExecutors.length != 2) {
+			throw new ExecutionPlanValidationException("time-length-window must have exactly two arguments");
+		}
+				
+		if (attributeExpressionExecutors[0].getReturnType() == Attribute.Type.INT) {
+            timeInMilliSeconds = (Integer) ((ConstantExpressionExecutor) attributeExpressionExecutors[0]).getValue();
+        } else if (attributeExpressionExecutors[0].getReturnType() == Attribute.Type.LONG) {
+            timeInMilliSeconds = (Long) ((ConstantExpressionExecutor) attributeExpressionExecutors[0]).getValue();
+        } else {
+            throw new ExecutionPlanValidationException("Time window's parameter attribute should be either int or long, but found " + attributeExpressionExecutors[0].getReturnType());
+        }
+		
+		length = (Integer) ((ConstantExpressionExecutor) attributeExpressionExecutors[1]).getValue();
+	}
+
+    @Override
+    protected synchronized void process(ComplexEventChunk<StreamEvent> streamEventChunk, Processor nextProcessor, StreamEventCloner streamEventCloner) {
+//    	System.out.println(streamEventChunk);
+    	
+    	while (streamEventChunk.hasNext()) {
+    		long currentTime = executionPlanContext.getTimestampGenerator().currentTime();
+            StreamEvent streamEvent = streamEventChunk.next();
+            StreamEvent clonedEvent = streamEventCloner.copyStreamEvent(streamEvent);
+            clonedEvent.setType(StreamEvent.Type.EXPIRED);
+            
+            if (streamEvent.getType() == StreamEvent.Type.CURRENT) {
+                clonedEvent.setTimestamp(currentTime + timeInMilliSeconds);
+            }
+
+            boolean eventScheduled = false;
+            while (expiredEventChunk.hasNext()) {
+                StreamEvent expiredEvent = expiredEventChunk.next();
+                long timeDiff = expiredEvent.getTimestamp() - currentTime;
+                if (timeDiff <= 0) {
+                    expiredEventChunk.remove();
+                    count--;
+                    System.out.println(count);
+                    streamEventChunk.insertBeforeCurrent(expiredEvent);
+                } else {
+                    scheduler.notifyAt(expiredEvent.getTimestamp());
+                    expiredEventChunk.reset();
+                    eventScheduled = true;
+                    break;
+                }
+            }
+
+            if (streamEvent.getType() == StreamEvent.Type.CURRENT) {
+            	if (count < length) {
+	            	count++;
+	            	System.out.println(count);
+	                this.expiredEventChunk.add(clonedEvent);
+            	} else {
+                    StreamEvent firstEvent = this.expiredEventChunk.poll();
+                    if (firstEvent != null) {
+                        streamEventChunk.insertBeforeCurrent(firstEvent);
+                        this.expiredEventChunk.add(clonedEvent);
+                    } else {
+                        streamEventChunk.insertBeforeCurrent(clonedEvent);
+                    }
+                }
+            
+                if (!eventScheduled) {
+                    scheduler.notifyAt(clonedEvent.getTimestamp());
+                }
+            }
+            expiredEventChunk.reset();
+        }
+    	
+//    	System.out.println(expiredEventChunk);
+        nextProcessor.process(streamEventChunk);
     }
 
     @Override
-    public void constructStreamEventPopulater(MetaStreamEvent metaStreamEvent, int streamEventChainIndex) {
-        super.constructStreamEventPopulater(metaStreamEvent, streamEventChainIndex);
-        timeWindow.constructStreamEventPopulater(metaStreamEvent, streamEventChainIndex);
-        lengthWindow.constructStreamEventPopulater(metaStreamEvent, streamEventChainIndex);
+    public synchronized StreamEvent find(ComplexEvent matchingEvent, Finder finder) {
+        return finder.find(matchingEvent, expiredEventChunk,streamEventCloner);
     }
 
     @Override
-    public void setStreamEventCloner(StreamEventCloner streamEventCloner) {
-        super.setStreamEventCloner(streamEventCloner);
-        timeWindow.setStreamEventCloner(streamEventCloner);
-        lengthWindow.setStreamEventCloner(streamEventCloner);
-    }
-
-    @Override
-    protected void init(ExpressionExecutor[] attributeExpressionExecutors, ExecutionPlanContext executionPlanContext) {
-        // nothing to do here
-    }
-
-    @Override
-    protected void process(ComplexEventChunk<StreamEvent> streamEventChunk, Processor nextProcessor,
-            StreamEventCloner streamEventCloner) {
-        // nothing to do here
+    public Finder constructFinder(Expression expression, MetaComplexEvent metaComplexEvent, ExecutionPlanContext executionPlanContext, List<VariableExpressionExecutor> variableExpressionExecutors, Map<String, EventTable> eventTableMap, int matchingStreamIndex, long withinTime) {
+        return CollectionOperatorParser.parse( expression, metaComplexEvent, executionPlanContext, variableExpressionExecutors, eventTableMap, matchingStreamIndex, inputDefinition, withinTime);
     }
 
     @Override
     public void start() {
-        lengthWindow.start();
-        timeWindow.start();
+        //Do nothing
     }
 
     @Override
     public void stop() {
-        lengthWindow.stop();
-        timeWindow.stop();
+        //Do nothing
     }
 
     @Override
     public Object[] currentState() {
-        Object[] lengthState = lengthWindow.currentState();
-        Object[] timeState = timeWindow.currentState();
-        Object[] state = new Object[lengthState.length + timeState.length];
-        System.arraycopy(state, 0, lengthState, 0, lengthState.length);
-        System.arraycopy(state, lengthState.length, timeState, 0, timeState.length);
-        return state;
+        return new Object[]{expiredEventChunk, count};
     }
 
     @Override
     public void restoreState(Object[] state) {
-        lengthWindow.restoreState(Arrays.copyOfRange(state, 0, 2));
-        timeWindow.restoreState(Arrays.copyOfRange(state, 2, state.length));
+        expiredEventChunk = (ComplexEventChunk<StreamEvent>) state[0];
+        count = (Integer) state[1];
     }
-
-    @Override
-    public StreamEvent find(ComplexEvent matchingEvent, Finder finder) {
-        return timeWindow.find(matchingEvent, finder);
-    }
-
-    @Override
-    public Finder constructFinder(Expression expression, MetaComplexEvent metaComplexEvent,
-            ExecutionPlanContext executionPlanContext, List<VariableExpressionExecutor> variableExpressionExecutors,
-            Map<String, EventTable> eventTableMap, int matchingStreamIndex, long withinTime) {
-        return timeWindow.constructFinder(expression, metaComplexEvent, executionPlanContext,
-                variableExpressionExecutors, eventTableMap, matchingStreamIndex, withinTime);
-    }
-
 }
